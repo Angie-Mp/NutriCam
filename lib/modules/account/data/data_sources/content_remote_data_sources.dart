e1f24b4bf6a4ad8c0e5ad7ef1fab8a7c14e88431
Angie <amariscalp12@gmail.com>
2025-10-20 11:20:00 -0500

    v1-cambios

--- lib/modules/account/data/data_sources/content_remote_data_sources.dart
+++ lib/modules/account/data/data_sources/content_remote_data_sources.dart
@@ -1,6 +1,6 @@
 import 'package:NutriCam/modules/account/data/models/create_account_model.dart';
 import 'package:NutriCam/modules/account/data/models/create_objetive_user_model.dart';
-
+//
 abstract class AccountRepository {
   Future<void> createAccountUserImpl(CreateAccountUserModel user);
   Future<bool> loginUserImpl(String email, String password);
